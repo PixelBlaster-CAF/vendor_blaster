16d18c0e62b583b7ad85257082326c23b16fc7c7
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-07-13 23:38:18 +0530

    vendor: Build GameSpace

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
