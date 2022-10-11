14a85cd2dd5ef9470fdb52edd1f450cfe8b4484d
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-10-11 23:57:17 +0530

    vendor: Uprev to 6.0 - Uprev caf revisions as well - Change Gapps

--- target/product/gapps.mk
+++ target/product/gapps.mk
@@ -2,12 +2,4 @@
 ifeq ($(BUILD_GAPPS),GAPPS)
 $(call inherit-product, vendor/gms/products/gms.mk)
 
-# Common Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    vendor/blaster/overlay-gapps/common
-
-# Exclude RRO Enforcement
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
-    vendor/blaster/overlay-gapps
-
 endif
--- target/product/version.mk
+++ target/product/version.mk
@@ -24,12 +24,12 @@
 # This is the global AOSPA version flavor that determines the focal point
 # behind our releases. This is bundled alongside $(AOSPA_MINOR_VERSION)
 # and only changes per major Android releases.
-BLASTER_BUILD_VERSION := 5.2
+BLASTER_BUILD_VERSION := 6.0
 
 BUILD_GAPPS := Vanilla
 
-CAF_SYSTEM_TAG := LA.QSSI.12.0.r1-07600-qssi.0
-CAF_VENDOR_TAG := LA.UM.9.14.r1-19600.01-LAHAINA.QSSI12.0
+CAF_SYSTEM_TAG := LA.QSSI.13.0.r1-07100-qssi.0
+CAF_VENDOR_TAG := LA.UM.9.14.2.r1-02800-QCM6490.QSSI12.0
 
 # Build Date
 BUILD_DATE := $(shell date -u +%Y%m%d)
